61c010396db538c8ca47b138bac1d9e1a4b205e4
mohamed oukhlidja <mohamadoukhlidja@gmail.com>
2023-12-01 15:11:52 +0100

    create some variables

---
+++ Variables-and-Primitive-Types/primitives.js
@@ -0,0 +1,30 @@
+//To declear a variable in javascript we use let or var or const for constant
+
+/*********  Strings  *********/
+var city = "Toranto";
+let country;
+const name = "Mohamed"; //const can not be reassigned
+// in javascript you can reassignt the value of a variable
+city = "London";
+country = "India";
+
+/*********  Numbers   *********/
+var age = 28;
+let year;
+const price = 1.5; //const can not be reassigned
+// in javascript you can reassignt the value of a variable
+age = 40;
+year = 2023;
+
+/*********  Booleans  *********/
+// there is two value for booleans variables True or Fales
+
+const happy = true;
+let sad = false;
+var broke = true;
+
+/*********  null and Undefined  *********/
+const empty = null
+var emptyToo = undefined
+
+
