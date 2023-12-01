52fd5a7bc4acb24da99602f0295e0b0b326268dc
mohamed oukhlidja <mohamadoukhlidja@gmail.com>
2023-12-01 15:34:51 +0100

    done the problem number one

---
+++ Variables-and-Primitive-Types/problem-set-wramup.js
@@ -0,0 +1,20 @@
+//    Create a constant variable of each of the following primitive types:
+//    - String
+const name = "mohamed";
+//    - Number
+const age = 24;
+//    - Boolean
+const learningJavascript = true;
+//    - Null
+const doYouLikeIt = null;
+//    - Undefined
+const doYouEnjoyIt = undefined;
+
+//    Use console.log to log out each variable to the terminal at the end.Fƒ
+console.log(name, age, learningJavascript, doYouLikeIt, doYouEnjoyIt);
+
+/*   HINT1: You can run your file in the terminal using this command: node <file_name>
+          For example, if your file is called ps1.js, you would type: node ps1.js
+
+   HINT2: Make sure your terminal is "looking at" the correct folder where your code is
+*/
---
+++ Variables-and-Primitive-Types/problem1.js
@@ -0,0 +1,14 @@
+//    1. Create a variable called name that points at a string containing your name
+let name = "mohamed";
+console.log(name);
+//    2. On the next line, re-assign the variable above to your favourite musician's name
+name = "tinarwein";
+console.log(name);
+//    3. On the next line, re-assign the variable again to the value: true
+name = true;
+console.log(name);
+//    3. On the next line, re-assign the variable one last time to the value: 42
+name = 42;
+console.log(name);
+
+//    After each of the above steps, log out the variable to the terminal with console.log.
