340bdbc66ad3b8edcf7526cccde4a746681df492
mohamed oukhlidja <mohamadoukhlidja@gmail.com>
2024-01-08 12:25:50 +0100

    problem set 2 is done

--- Conditional Statements/problem-set-2.js
+++ Conditional Statements/problem-set-2.js
@@ -7,3 +7,11 @@
 
     HINT: modulus operator ( % ) is your friend
 */
+
+/* *********w solutions *********** */
+let num = 353;
+if (num % 2 ===0) {
+    console.log('the number is even');
+}else{
+    console.log('the number is odd');
+}
