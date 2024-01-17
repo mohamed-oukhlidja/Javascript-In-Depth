2f2b92116c53d4f54ea216434a94a7f650a9f3a9
mohamed oukhlidja <mohamadoukhlidja@gmail.com>
2024-01-17 12:34:02 +0100

    created the countdown function

---
+++ While Loops/Countdown.js
@@ -0,0 +1,12 @@
+// Write a function that takes a number as a parameter and uses a while loop to count down from that number to 1. Log each number to the console.
+
+function countdown(n) {
+  // Your code here
+  while (n >= 1) {
+    console.log(n);
+    n--;
+  }
+}
+
+// Example
+countdown(5); // Should log: 5, 4, 3, 2, 1
