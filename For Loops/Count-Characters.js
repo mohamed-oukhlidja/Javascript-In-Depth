8e492054e640ca8e04edf0c0863b5da133acb56e
mohamed oukhlidja <mohamadoukhlidja@gmail.com>
2024-01-31 21:12:40 +0100

    count charchters is done

--- For Loops/Count-Characters.js
+++ For Loops/Count-Characters.js
@@ -1,1 +1,10 @@
 // Write a function that takes a string as a parameter and uses a for loop to count and return the number of characters in the string.
+function numberOfChar(string) {
+  let nume =0;
+  for (let i = 0; i < string.length; i++) {
+    nume++;
+  }
+  return nume;
+}
+
+console.log(numberOfChar("hello"));
