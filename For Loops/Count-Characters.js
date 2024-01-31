082c76278e0c54a255a9411d0894ddce97b7262d
mohamed oukhlidja <mohamadoukhlidja@gmail.com>
2024-01-31 21:24:51 +0100

    added a test for the code

--- For Loops/Count-Characters.js
+++ For Loops/Count-Characters.js
@@ -1,10 +1,33 @@
 // Write a function that takes a string as a parameter and uses a for loop to count and return the number of characters in the string.
-function numberOfChar(string) {
-  let nume =0;
+function CountCharacters(string) {
+  let numeOfCharacters = 0;
   for (let i = 0; i < string.length; i++) {
-    nume++;
+    numeOfCharacters++;
   }
-  return nume;
+  return numeOfCharacters;
 }
 
-console.log(numberOfChar("hello"));
+// Simple test cases
+function testCountCharacters() {
+  let result = CountCharacters("Hello world");
+  let resultExpected = 11;
+  if (result === resultExpected) {
+    console.log("Test passed for 'Hello world'");
+  } else {
+    console.error(
+      `Test failed for 'Hello, World!'. Expected ${resultExpected}, but got ${result}`
+    );
+  }
+
+  result = CountCharacters("");
+  resultExpected = 0;
+  if (result === resultExpected) {
+    console.log("Test passed for an empty string");
+  } else {
+    console.error(
+      `Test passed for an empty string. Expected ${resultExpected}, but got ${result}`
+    );
+  }
+}
+
+testCountCharacters();
